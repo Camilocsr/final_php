e8f428259d2e5b722b812a9ba008d692a9f7291c
Camilocsr <esmunred@gmail.com>
2022-10-30 11:19:33 -0500

    ingreso trabajo final php

---
+++ includes/_footer.php
@@ -0,0 +1,56 @@
+<footer class="text-center text-lg-start bg-dark text-muted">
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4  border-bottom">
+
+    <div class="me-3 d-none d-lg-block">
+      <span>Sigueme en mis redes sociales como camilosolanorodriguez o preciona en los icons de las redes sociales.</span>
+    </div>
+        <a href="https://www.facebook.com/camilosolanorodriguez/" class="me-4 text-reset">
+          <i class="bi bi-facebook"></i>
+        </a>
+        <a href="https://twitter.com/camilosolanoro" class="me-4 text-reset">
+          <i class="bi bi-twitter"></i>
+        </a>
+        <a href="https://github.com/Camilocsr" class="me-4 text-reset">
+        <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.instagram.com/camilosolanoro/" class="me-4 text-reset">
+          <i class="bi bi-instagram"></i>
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=573117022824&text=Te%20escribo%20por%20el%20motivo%20de..." class="me-4 text-reset">
+          <i class="fa-brands fa-whatsapp"></i>
+        </a>
+    </div>
+  </section>
+    <div class="container text-center text-md-start mt- bg-dark">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Feliz dia: ❤️👌
+          </h6>
+          <p>
+            Estudiante de la universitaria de Colombia.
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contactame:
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Bogota D.C</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            esmunred@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +57 313 636 8783</p>
+        </div>
+      </div>
+    </div>
+    
+  <div class="text-center bd-dark">
+    2022 Camilo Solano Rodriguez... 
+  </div>
+  </section>
+
+
+</footer>
+  </body>
